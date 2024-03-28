f3fcd065e35a14f8ff3effdda26c3dd807868327
MoeHeinKhant005 <moeheink578@gmail.com>
2024-03-28 12:13:03 +0630

    Project Setup
    Set up project and removed unnecessary codes and files. Imported icons, google fonts and colors. Also added custom fonts and colors to tailwind config.

--- README.md
+++
@@ -1,2 +0,0 @@
-# Todo-List-App
-Todo List App made with React and TailwindCSS
---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <h2 className="text-primary font-lexend text-3xl">Hello World</h2>
+  )
+}
+
+export default App;
