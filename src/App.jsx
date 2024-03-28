1bd5da9d2e5d941b2c2c8f2b8c57714c8f56c6b7
MoeHeinKhant005 <moeheink578@gmail.com>
2024-03-28 12:25:54 +0630

    Add Container component
    Added Container.jsx and its stylings. Also imported it inside App.jsx.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import Container from './components/Container';
+
 function App() {
   return (
     <div className="w-full h-screen bg-primary flex flex-row justify-center items-center">
-      
+      <Container/>
     </div>
   )
 }
---
+++ src/components/Container.jsx
@@ -0,0 +1,9 @@
+const Container = () => {
+    return (
+        <div className="w-[85%] h-auto bg-white flex flex-col justify-evenly items-center rounded">
+            <h3 className="title text-primary font-lexend font-semibold text-3xl self-start mx-6">ToDo</h3>
+        </div>
+    )
+}
+
+export default Container;
