64ba8bd5a4f64459ab8baf7e64265b628ae15f83
MoeHeinKhant005 <moeheink578@gmail.com>
2024-03-28 14:56:16 +0630

    Add TodoList component
    Added TodoList.jsx component and its stylings. Also imported it inside Container.jsx.

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,10 +1,12 @@
 import TodoForm from './TodoForm';
+import TodoList from './TodoList';
 
 const Container = () => {
     return (
         <div className="w-[85%] h-auto bg-white flex flex-col justify-evenly items-center rounded">
             <h3 className="title text-primary font-lexend font-semibold text-3xl self-start mx-6">ToDo</h3>
             <TodoForm/>
+            <TodoList/>
         </div>
     )
 }
