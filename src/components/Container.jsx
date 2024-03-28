4fb9528e1ef5284d67028f85c4f0544229a1ab4a
MoeHeinKhant005 <moeheink578@gmail.com>
2024-03-28 15:16:40 +0630

    Add spacing and overflow scrolling
    Added spacings between components and scroll when items are overflowing.

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -3,7 +3,7 @@ import TodoList from './TodoList';
 
 const Container = () => {
     return (
-        <div className="w-[85%] h-auto bg-white flex flex-col justify-evenly items-center rounded">
+        <div className="w-[85%] h-auto bg-white flex flex-col justify-evenly items-center rounded gap-y-4 py-6">
             <h3 className="title text-primary font-lexend font-semibold text-3xl self-start mx-6">ToDo</h3>
             <TodoForm/>
             <TodoList/>
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,9 +1,9 @@
 const TodoList = () => {
     return (
-        <div className="w-[85%] h-auto flex flex-col justify-start items-center gap-y-2">
-            <div className="todo-item w-full h-[50px] bg-white flex flex-row justify-between items-center gap-x-2 px-2">
+        <div className="w-[85%] h-auto max-h-[300px] flex flex-col justify-start items-center gap-y-2 overflow-y-auto">
+            <div className="todo-item w-full h-[50px] bg-white flex flex-none flex-row justify-between items-center gap-x-2 px-4">
                 <input type="checkbox" className="w-[5%] accent-primary text-base"/>
-                <span className="w-[80%] font-dm-sans text-sm text-slate-900 text-ellipsis overflow-hidden whitespace-nowrap"></span>
+                <span className="w-[80%] font-dm-sans text-sm text-slate-900 text-ellipsis overflow-hidden whitespace-nowrap">To play some games</span>
                 <i className="fa-solid fa-trash text-slate-600 text-base w-[5%]"></i>
             </div>
         </div>
