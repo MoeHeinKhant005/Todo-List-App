d5e52a59ec886e43a20b5d7bfd35a19cd72f37d9
MoeHeinKhant005 <moeheink578@gmail.com>
2024-03-29 07:16:59 +0630

    Add funcs to create new item
    Added functionalities to create new todo item whenever a user has clicked the Add button and changed the state data using setTodoData function.

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,18 +1,29 @@
-import { useContext } from 'react'; 
+import { useContext, createRef } from 'react'; 
 import { TodoContext } from '../App';
 
 const TodoForm = () => {
 
     const [todoData, setTodoData] = useContext(TodoContext);
+    const todoInputRef = createRef();
+
+    function handleAdd(){
+        const newTodo = {
+            task: todoInputRef.current.value,
+            id: todoData.length + 1
+        };
+        setTodoData([...todoData], newTodo);
+        todoInputRef.current.value = ""
+    }
 
     return (
         <div className="w-[85%] h-auto flex flex-row justify-center items-center">
             <input 
             type="text"
+            ref={todoInputRef}
             placeholder="Enter a new task..."
             className="w-4/5 h-[50px] bg-slate-100 text-slate-800 font-dm-sans text-sm rounded-s px-5 border-none outline-none"
             />
-            <button className="w-1/5 h-[50px] bg-primary text-white rounded-e">
+            <button className="w-1/5 h-[50px] bg-primary text-white rounded-e" onClick={handleAdd}>
                 <i className="fa-solid fa-circle-plus"></i>
             </button>
         </div>
