3ce4f7f9baa249e15e43289f691996f97f8d7f99
MoeHeinKhant005 <moeheink578@gmail.com>
2024-03-29 07:33:44 +0630

    Add handleCheck and handleDelete funcs
    Added handleCheck function to handle checked todo items and handleDelete function to handle deleted todo items.

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -5,13 +5,26 @@ const TodoList = () => {
 
     const [todoData, setTodoData] = useContext(TodoContext);
 
+    function handleCheck(event, id){
+        event.target.nextElementSibling.classList.add('line-through');
+        setTimeout(() => {
+            const updatedData = todoData.filter(todo => todo.id !== id);
+            setTodoData(updatedData);
+        }, 5000);
+    }
+
+    function handleDelete(id){
+        const updatedData = todoData.filter(todo => todo.id !== id);
+        setTodoData(updatedData);
+    }
+
     return (
         <div className="w-[85%] h-auto max-h-[300px] flex flex-col justify-start items-center gap-y-2 overflow-y-auto">
             { todoData.length >= 1 ? todoData.map(todo => (
             <div key={ todo.id } className="todo-item w-full h-[50px] bg-white flex flex-none flex-row justify-between items-center gap-x-2 px-4">
-                <input type="checkbox" className="w-[5%] accent-primary text-base"/>
+                <input type="checkbox" className="w-[5%] accent-primary text-base" onChange={(e) => handleCheck(e, todo.id)}/>
                 <span className="w-[80%] font-dm-sans text-sm text-slate-900 text-ellipsis overflow-hidden whitespace-nowrap">{ todo.task }</span>
-                <i className="fa-solid fa-trash text-slate-600 text-base w-[5%]"></i>
+                <i className="fa-solid fa-trash text-slate-600 text-base w-[5%]" onClick={() => handleDelete(todo.id)}></i>
             </div>
             )): <p className='font-dm-sans text-sm text-gray-400 my-4'>No todo list has added yet!</p>}
         </div>
